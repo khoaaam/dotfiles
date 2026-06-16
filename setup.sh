6550fa45ae9d1ba82ccacbe1af6386ccd03c08a6
Moritz Knueppelholz <104736021+khoaaam@users.noreply.github.com>
2026-06-16 14:36:11 +0200

    Update setup.sh to use HOME variable

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 
-mkdir -p ~/.config
+mkdir -p $HOME/.config
 
-ln -sf ~/dotfiles/starship/starship.toml ~/.config/starship.toml
+ln -sf $HOME/dotfiles/starship/starship.toml ~/.config/starship.toml
 
-echo 'eval "$(starship init bash)"' >> ~/.bashrc
+echo 'eval "$(starship init bash)"' >> $HOME/.bashrc
 echo "starship erfolgreich geladen"
